dcecec6bb5a46a9974c2f06d6e08c47915c928f7
Quentin PLESSIS <xqu-plessis@tsm.kddilabs.jp>
2013-08-28 13:41:56 +0900

    Fixed bug related to separation of tests and implementation

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -20,7 +20,6 @@ describe "User pages" do
   end
   
   describe "signup" do
-
     before { visit signup_path }
 
     let(:submit) { "Create my account" }
@@ -39,13 +38,8 @@ describe "User pages" do
     end
 
     describe "with valid information" do
-	  let(:email) { "user@example.com" }
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: email
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
+	  let(:user) { FactoryGirl.build(:user) }
+      before { valid_signup(user) }
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
@@ -53,11 +47,10 @@ describe "User pages" do
 	  
 	  describe "after saving the user" do
         before { click_button submit }
-        let(:user) { User.find_by(email: email) }
 
 		it { should have_link('Sign out') }
         it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_success_message('Welcome') }
       end
     end
   end
